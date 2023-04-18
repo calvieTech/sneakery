826371752dd698fd2328c541a69264542241da46
Calvin Thang <calviethang@Calvins-MacBook-Pro.local>
2023-04-18 16:06:07 -0400

    changed prod config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -4,4 +4,9 @@ const common = require("./webpack.common.config");
 
 module.exports = merge(common, {
 	mode: "production",
+	devtool: "source-map",
+	optimization: {
+		minimize: true,
+		minimizer: [new TerserPlugin()],
+	},
 });
